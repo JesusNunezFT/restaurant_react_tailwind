781f9b3bc3db1018d1ec9651b319fa1b884bff65
Jesus Nuñez <jesus.nunez@theconsultingcompany.eu>
2023-07-29 21:35:14 -0500

    Primer commit ajustando proyecto y haciendo la vista principal

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import background from "./imagen/imagen2.png";
+import logo from "./imagen/logo1-removebg-preview.png";
+
+export default function App() {
+  return (
+    <div>
+      <nav className="flex justify-center items-center max-w-full bg-primary h-28 p-1 ">
+        <img className="w-56 h-28" src={logo} alt="logo" />
+      </nav>
+      <div
+        className="Box flex flex-col justify-start items-center  min-h-screen max-w-7xl "
+        style={{ backgroundImage: `url(${background})`, filter: "brightness(90%)" }} 
+      >
+        <button className="bg-secundary mt-10 mb-7 p-2 w-36 h-10 rounded-3xl shadow-md text-primary font-medium text-lg">Menú</button>
+        <button className="bg-primary m-7 mb-7 p-2 w-60 h-16 rounded-3xl shadow-md text-secundary font-medium text-2xl">Entadas</button>
+        <button className="bg-primary m-7 mb-7 p-2 w-60 h-16 rounded-3xl shadow-md text-secundary font-medium text-2xl">Pecaditos</button>
+        <button className="bg-primary m-7 mb-7 p-2 w-60 h-16 rounded-3xl shadow-md text-secundary font-medium text-2xl">Bebidas</button>
+        <button className="bg-primary m-7 mb-7 p-2 w-60 h-16 rounded-3xl shadow-md text-secundary font-medium text-2xl">Especiales</button>
+       
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { 
+    // screens: {
+    //   'sm': '640px',
+    //   'md': '768px',
+    //   'lg': '1024px',
+    //   'xl': '1280px',
+    //   '2xl': '1536px',
+    // },       
+      colors: {
+      'primary': '#003D3E',
+      'secundary': '#E9D5BA',    
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    // fontFamily: {
+    //   sans: ['Graphik', 'sans-serif'],
+    //   serif: ['Merriweather', 'serif'],
+    // },
+    extend: {
+      // spacing: {
+      //   '8xl': '96rem',
+      //   '9xl': '128rem',
+      // },
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // }
+    },
+  },
+  plugins: [],
+ 
+}
+
+
+
+
