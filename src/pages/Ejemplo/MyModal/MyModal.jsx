d861283b8644d8c75e5042fdb4210e4f61deaef6
Eduardo Romero <eduardo.romero@dualbootpartners.com>
2023-08-04 22:19:38 -0300

    improving MyModal

--- src/pages/Ejemplo/MyModal/MyModal.jsx
+++ src/pages/Ejemplo/MyModal/MyModal.jsx
@@ -1,24 +1,14 @@
 const MyModal = ({ handleModal, imageSelected }) => {
   return (
     <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        background: "rgba(0, 0, 0, 0.8)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80 flex justify-center items-center"
       onClick={handleModal}
     >
       <img
         src={imageSelected}
         alt="Imagen en modal"
-        style={{ maxHeight: "90vh", maxWidth: "90vw" }}        
-      />,
+        className="max-h-[90vh] max-w-[90vw] rounded-2xl"
+      />
     </div>
   );
 };
